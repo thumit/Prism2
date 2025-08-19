f312634b4cbc4568be871daedf42baf092485a63
Tuan Dung Nguyen <dzung.csu@gmail.com>
2025-08-19 13:23:32 -0600

    new version 2.0.09 but with solving time change

--- src/prism_root/Prism2Main.java
+++ src/prism_root/Prism2Main.java
@@ -94,7 +94,7 @@ public class Prism2Main extends JFrame {
 	private MenuItem_SetTransparency 	setTransparency;		// For menuWindow
 	private MenuItem_CaptureGUI 		captureGUI;				// For menuWindow
 		
-	private static String 				prism_version = "PRISM ALPHA 2.0.08";
+	private static String 				prism_version = "PRISM ALPHA 2.0.09";
 	private String 						currentProject;
 	private static PrismDesktopPane 	prism_DesktopPane;
 	private static PrismContentPane 	prism_ContentPane;
